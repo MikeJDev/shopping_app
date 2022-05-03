d983723ad23ec57915086dc1186cf2fdd55894de
Mike J <mjanes@fischerhomes.com>
2022-05-03 11:36:20 -0400

    create get and post route from the router class

---
+++ routes.js/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const controllers = require("../controllers");
+const router = express.Router();
+
+// create as get and post route for / 
+router
+  .route("/")
+  .get(controllers.getAllItems)
+  .post(controllers.createItem);
+router
+  .route("/:id")
+  .put(controllers.updateItem)
+  .delete(controllers.deleteItem);
+module.exports = router;
