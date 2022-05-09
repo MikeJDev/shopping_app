4419a8475dab5766eb8b96006a329cde503a67f4
Mike J <mjanes@fischerhomes.com>
2022-05-09 19:34:35 -0400

    fixed database creation on startup

--- services/db.js
+++ services/db.js
@@ -4,30 +4,37 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'shoppingDb'
+  // database: 'shoppingDb'
 });
 
 connection.connect(function(err) {
   if (err) {
     return console.error('error: ' + err.message);
   }
-  console.log('Connected to the DB.');
-
+  console.log('Connected to the Mysql server.');
   connection.query("CREATE DATABASE IF NOT EXISTS shoppingDb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-
-  var sql = `CREATE TABLE IF NOT EXISTS items (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) not null,
-    description VARCHAR(255),
-    quantity INTEGER not null,
-    isPurchased BOOLEAN DEFAULT false
-    )`;
-  connection.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Items table created");
+    if (err) {
+      return console.error('error creating db: ' + err.message);
+    };
+  console.log("Database created");
+    connection.changeUser({
+      database : 'shoppingDb'
+    }, function(err) {
+      if (err) {
+        console.error('error in changing database', err);
+        return;
+      }});
+    const sql = `CREATE TABLE IF NOT EXISTS items (
+      id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(50) not null,
+      description VARCHAR(255),
+      quantity INTEGER not null,
+      isPurchased BOOLEAN DEFAULT false
+      )`;
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Items table created");
+    });
   });
 });
 
